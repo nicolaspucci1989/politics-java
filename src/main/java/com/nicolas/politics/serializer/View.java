6428b35d56c77d006a1eb762611e68ec5cb539ff
Nicolas <nicolaspucci1989@gmail.com>
2021-11-17 01:00:33 -0300

    cambios menores

--- src/main/java/com/nicolas/politics/serializer/View.java
+++ src/main/java/com/nicolas/politics/serializer/View.java
@@ -1,9 +1,9 @@
 package com.nicolas.politics.serializer;
 
 public class View {
-    public static interface Zona {
-        public static interface Grilla {}
+    public interface Zona {
+        interface Grilla {}
 
-        public static interface Plana {}
+        interface Plana {}
     }
 }
